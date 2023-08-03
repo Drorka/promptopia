2aecd2083c5d7f2029b8006815116e286ee8346d
Drorka~ <drorpa4@gmail.com>
2023-08-03 17:05:18 +0300

    fix login bugs

--- models/user.js
+++ models/user.js
@@ -9,11 +9,10 @@ const UserSchema = new Schema({
 	username: {
 		type: String,
 		required: [true, "Username is required."],
-		unique: [true, "Username already exists."],
-		match: [
-			/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
-			"Username invalid, it should contain 8-20 alphanumeric letters and be unique!",
-		],
+		// match: [
+		// 	/^(?=.{4,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+		// 	"Username invalid, it should contain 4-20 alphanumeric letters!",
+		// ],
 	},
 	image: {
 		type: String,
